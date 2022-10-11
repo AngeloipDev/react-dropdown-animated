fb3912282aba3c6dbbcb157cdcdc4d3262b28cbc
AngeloIngaJP <angelo.inga@unmsm.edu.pe>
2022-10-11 18:38:47 -0500

    react-dropdown-animated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import user from "./user.jpg";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import { FaRegEdit } from "react-icons/fa";
+import { AiOutlineSetting, AiOutlineMail } from "react-icons/ai";
+import { FiHelpCircle } from "react-icons/fi";
+import { BiLogOut } from "react-icons/bi";
+import { useRef } from "react";
 
 function App() {
+  const ref = useRef(null);
+  const handleClass = () => {
+    ref.current.classList.toggle("active");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="action">
+      <div className="profile" onClick={handleClass}>
+        <img src={user} alt="" />
+      </div>
+      <div ref={ref} className="menu">
+        <h3>
+          Someone Famous
+          <br />
+          <span>Website Designer</span>
+        </h3>
+        <ul>
+          <li>
+            <HiOutlineUserCircle className="icon" />
+            <a href="#">My Profile</a>
+          </li>
+          <li>
+            <FaRegEdit className="icon" />
+            <a href="#">Edit Profile</a>
+          </li>
+          <li>
+            <AiOutlineMail className="icon" />
+            <a href="#">Inbox</a>
+          </li>
+          <li>
+            <AiOutlineSetting className="icon" />
+            <a href="#">Settings</a>
+          </li>
+          <li>
+            <FiHelpCircle className="icon" />
+            <a href="#">Help</a>
+          </li>
+          <li>
+            <BiLogOut className="icon" />
+            <a href="#">Logout</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
